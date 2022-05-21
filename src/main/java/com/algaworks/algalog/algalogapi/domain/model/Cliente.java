40792acaa1ed0e8cca49eb117216d58d828da992
cesarionto <cesariopereiraneto@gmail.com>
2022-05-21 13:20:36 -0300

    mapeando a entidade cliente no JPA

--- src/main/java/com/algaworks/algalog/algalogapi/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/algalogapi/domain/model/Cliente.java
@@ -1,13 +1,30 @@
 package com.algaworks.algalog.algalogapi.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "cliente")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cliente {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 }
