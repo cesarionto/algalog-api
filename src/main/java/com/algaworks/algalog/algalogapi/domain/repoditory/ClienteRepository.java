7646f308dee46f45abce738b79e7b1a5bf4dadd5
cesarionto <cesariopereiraneto@gmail.com>
2022-05-21 13:28:28 -0300

    crando o repository de cliente

---
+++ src/main/java/com/algaworks/algalog/algalogapi/domain/repoditory/ClienteRepository.java
@@ -0,0 +1,13 @@
+package com.algaworks.algalog.algalogapi.domain.repoditory;
+
+import java.util.List;
+
+import com.algaworks.algalog.algalogapi.domain.model.Cliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+    List<Cliente> findByNome(String nome);
+}
