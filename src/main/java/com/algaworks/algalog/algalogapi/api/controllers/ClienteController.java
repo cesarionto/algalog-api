245e6c33028794b7d5b26af3cf9322fa477c4dac
cesarionto <cesariopereiraneto@gmail.com>
2022-05-21 13:48:34 -0300

    resources

--- src/main/java/com/algaworks/algalog/algalogapi/api/controllers/ClienteController.java
+++ src/main/java/com/algaworks/algalog/algalogapi/api/controllers/ClienteController.java
@@ -1,14 +1,32 @@
 package com.algaworks.algalog.algalogapi.api.controllers;
 
+import java.util.List;
+
+import com.algaworks.algalog.algalogapi.domain.model.Cliente;
+import com.algaworks.algalog.algalogapi.domain.repoditory.ClienteRepository;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping(value = "/clientes")
+@AllArgsConstructor
 public class ClienteController {
 
+    ClienteRepository clienteRepository;
 
     @GetMapping
-    public void String(){}
+    public List<Cliente> String() {
+        return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long id) {
+        return clienteRepository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
 }
